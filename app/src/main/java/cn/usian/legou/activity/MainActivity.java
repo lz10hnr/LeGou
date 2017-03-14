4c54e6705ac55199b1934f2c7bd58fe279d2e7d6
lz10hnr <923622583@qq.com>
2017-03-14 18:15:06 +0800

    3月14号更改内容

--- app/src/main/java/cn/usian/legou/activity/MainActivity.java
+++ app/src/main/java/cn/usian/legou/activity/MainActivity.java
@@ -1,10 +1,10 @@
 package cn.usian.legou.activity;
 
+import android.os.Bundle;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.widget.CompoundButton;
+import android.view.View;
 import android.widget.RadioButton;
 
 import cn.usian.legou.R;
@@ -13,7 +13,7 @@ import cn.usian.legou.common.Keys;
 import cn.usian.legou.fragment.EmptyFragment;
 import cn.usian.legou.fragment.HomeFragment;
 
-public class MainActivity extends AppCompatActivity implements RadioButton.OnCheckedChangeListener {
+public class MainActivity extends AppCompatActivity implements RadioButton.OnClickListener {
 
     RadioButton homeBtn;
     RadioButton foundBtn;
@@ -48,10 +48,10 @@ public class MainActivity extends AppCompatActivity implements RadioButton.OnChe
 
         //我判断是否已经添加过 y 显示 n 添加 隐藏上一个fragment
         if (!fragment.isAdded()) {
-            transaction.add(R.id.contentGroup, homeFragment);
-        }else{
-            transaction.show(fragment);
+            transaction.add(R.id.contentGroup, fragment);
         }
+        transaction.show(fragment);
+
         transaction.commit();
 
         currentFragment=fragment;
@@ -66,35 +66,34 @@ public class MainActivity extends AppCompatActivity implements RadioButton.OnChe
         foundBtn= (RadioButton) findViewById(R.id.foundBtn);
         shoppingCarBtn=(RadioButton)findViewById(R.id.shoppingCarBtn);
         mineButtoon=(RadioButton)findViewById(R.id.mineBtn);
-        homeBtn.setOnCheckedChangeListener(this);
-        foundBtn.setOnCheckedChangeListener(this);
-        shoppingCarBtn.setOnCheckedChangeListener(this);
-        mineButtoon.setOnCheckedChangeListener(this);
+        homeBtn.setOnClickListener(this);
+        foundBtn.setOnClickListener(this);
+        shoppingCarBtn.setOnClickListener(this);
+        mineButtoon.setOnClickListener(this);
         currentFragment=homeFragment;
         changeContentView(homeFragment,null,false);
     }
 
     @Override
-    public void onCheckedChanged(CompoundButton compoundButton, boolean b) {
+    public void onClick(View view) {
         Bundle bundle=new Bundle();
-        switch (compoundButton.getId()){
+        switch (view.getId()){
             case R.id.homeBtn:
                 bundle.putString(Keys.FRAGMENTNAME,"home");
                 changeContentView(homeFragment,bundle,false);
                 break;
             case R.id.foundBtn:
-                bundle.putString(Keys.FRAGMENTNAME,"home");
+                bundle.putString(Keys.FRAGMENTNAME,"found");
                 changeContentView(foundFragment,bundle,false);
                 break;
             case R.id.shoppingCarBtn:
-                bundle.putString(Keys.FRAGMENTNAME,"home");
+                bundle.putString(Keys.FRAGMENTNAME,"shopping");
                 changeContentView(shoppingFragment,bundle,false);
                 break;
             case R.id.mineBtn:
-                bundle.putString(Keys.FRAGMENTNAME,"home");
+                bundle.putString(Keys.FRAGMENTNAME,"mine");
                 changeContentView(mineFragment,bundle,false);
                 break;
-
         }
     }
 }
