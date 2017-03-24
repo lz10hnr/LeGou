86997307648af1f0964d5e211d7c7b0a76cc4cf9
lz10hnr <923622583@.com>
2017-03-24 15:23:49 +0800

    3月24号修改

--- app/src/main/java/cn/usian/legou/base/BaseActivity.java
+++ app/src/main/java/cn/usian/legou/base/BaseActivity.java
@@ -2,18 +2,30 @@ package cn.usian.legou.base;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.FrameLayout;
+
+import cn.usian.legou.R;
 
 public abstract class BaseActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(layoutId());
+        setContentView(R.layout.activity_base);
+        initDrawerLayout();
         App.context = this;
         initView();
         initData();
         initListener();
         loadData();
+    }
+
+    private void initDrawerLayout(){
+        FrameLayout contentLayout = (FrameLayout) findViewById(R.id.contentLayout);
+        View view = LayoutInflater.from(this).inflate(layoutId(), null);
+        contentLayout.addView(view);
     }
 
     protected abstract int layoutId();
---
+++ app/src/main/java/cn/usian/legou/fragment/LeftFragment.java
@@ -0,0 +1,37 @@
+package cn.usian.legou.fragment;
+
+import android.view.View;
+
+import cn.usian.legou.R;
+import cn.usian.legou.base.BaseFragment;
+
+/**
+ * Created by xingge on 2017/3/24.
+ */
+
+public class LeftFragment extends BaseFragment {
+    @Override
+    protected int getLayoutId() {
+        return R.layout.left_fragment;
+    }
+
+    @Override
+    protected void initView(View view) {
+
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void loadData() {
+
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+}
